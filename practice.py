301e82017fe753e4a0eb5153d44db0e25f53cd65
Cyberdragon <aditya5namdeo@gmail.com>
2026-01-09 19:42:16 +0530

    9 jan 26

--- practice.py
+++ practice.py
@@ -1,10 +1,24 @@
-def analyze_text(text):
-    print("reversed: ",text[::-1])
-    print("word count :" ,len(text.split()))
+import random 
+def start_game():
+    number_to_guess = random.randint(1,10)
+    attempts = 0
+    print("I'm thinking of a number between 1 and 10")
+    
 
+    while True:
+        try:
+            guess = int(input("Take a guess: "))
+            attempts += 1
+            
+            if guess < number_to_guess:
+                print("too low ! ")
+            elif guess > number_to_guess:
+                print("too high ! ")
+            else:
+                print(f"correct! you got it in {attempts} guesses.")
+                break
+        except ValueError:
+            print("please enter a valid number. ")
 
-# print(analyze_text("Python is fun"))
-print(str(35.33))
-
-x = lambda a, b : a - b
-print(x(5, 3))
+if __name__ == "__main__":
+    start_game()
