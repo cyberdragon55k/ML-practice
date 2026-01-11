aa314631fa9029c6b5c1c1809bfac9c279cfb885
Cyberdragon <aditya5namdeo@gmail.com>
2026-01-11 22:12:38 +0530

    11 jan 26

--- practice.py
+++ practice.py
@@ -1,27 +1,13 @@
-import random 
-def start_game():
-    number_to_guess = random.randint(1,10)
-    attempts = 0
-    print("I'm thinking of a number between 1 and 10")
-    
-
-    while True:
-        try:
-            guess = int(input("Take a guess: "))
-            attempts += 1
-            
-            if guess < number_to_guess:
-                print("too low ! ")
-            elif guess > number_to_guess:
-                print("too high ! ")
-            else:
-                print(f"correct! you got it in {attempts} guesses.")
-                break
-        except ValueError:
-            print("please enter a valid number. ")
-
-if __name__ == "__main__":
-    start_game()
+string = "A man, a plan, a canal: Panama!"
+string = string.replace(" ","")
+string = string.replace("'","")
+string = string.replace(",","")
+string = string.replace(":","")
+string = string.replace("!","")
+string=string.lower()
 
 
-    #ooooooo
+if string == string[::-1] :
+    print("the string is pallendrome")
+else:
+    print('the string is not a pallendrome')
