584f2c37d5f48af94e1f64a546ec7449e801b553
Cyberdragon <aditya5namdeo@gmail.com>
2026-01-19 23:07:25 +0530

    19 jan 26

--- practice.py
+++ practice.py
@@ -30,12 +30,33 @@
 
 
 
-list = [10,50,30]
-if 30 in list:
-    print('found it ')
+# list = [10,50,30]
+# if 30 in list:
+#     print('found it ')
 
-try:
-    idx = list.index(30)
-    print(f"index is {idx}")
-except ValueError:
-    print('not found ')
+# try:
+#     idx = list.index(30)
+#     print(f"index is {idx}")
+# except ValueError:
+#     print('not found ')
+
+def binary_search(arr, target):
+    low = 0 
+    high = len(arr) -1
+
+    while low <= high:
+        mid = (low+high) //2
+        guess = arr[mid]
+        if guess == target:
+            return mid
+        elif guess >target:
+            high = mid -1
+        else:
+            low = mid +1
+    return -1
+list = [1,2,3,5,7,9,11,55]
+
+print(binary_search(list,7))
+
+print(binary_search(list,55))
+
