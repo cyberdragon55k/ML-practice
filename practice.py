66a8944f6a23ff357812bcb964366945c575c6e3
Cyberdragon <aditya5namdeo@gmail.com>
2026-02-03 23:46:04 +0530

    3 feb 26

--- practice.py
+++ practice.py
@@ -172,8 +172,43 @@
 # arr = [3,2,2,2,2,2,2,2,2]
 # print(arr)
 
-rows = 5
-for i in range(1,rows +1):
-    print('#' * i)
+# rows = 5
+# for i in range(1,rows +1):
+#     print('#' * i)
 
-for i in range
+class Stack:
+  def __init__(self):
+    self.stack = []
+
+  def push(self, element):
+    self.stack.append(element)
+
+  def pop(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack.pop()
+
+  def peek(self):
+    if self.isEmpty():
+      return "Stack is empty"
+    return self.stack[-1]
+
+  def isEmpty(self):
+    return len(self.stack) == 0
+
+  def size(self):
+    return len(self.stack)
+
+# Create a stack
+myStack = Stack()
+
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+
+print("Stack: ", myStack.stack)
+print("Pop: ", myStack.pop())
+print("Stack after Pop: ", myStack.stack)
+print("Peek: ", myStack.peek())
+print("isEmpty: ", myStack.isEmpty())
+print("Size: ", myStack.size())
