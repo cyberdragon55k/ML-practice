65faed2b25a939d2f4927b104ebbbf8ca9da5a81
Cyberdragon <aditya5namdeo@gmail.com>
2026-02-10 21:00:24 +0530

    10 feb 46

--- practice.py
+++ practice.py
@@ -221,9 +221,10 @@
 
 
 list = [1,2,2,3,4,5,5,5]
-print(list[0])
-print(list[1])
-print(list[3])
-print(list[4])
-print(list[4])
-print(list[5])
+# print(list[0])
+# print(list[1])
+# print(list[3])
+# print(list[4])
+# print(list[4])
+
+print(list[6])
