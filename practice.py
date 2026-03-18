60a6fb74a7c52769313534cfcf15d7f859c5ff8a
Cyberdragon <aditya5namdeo@gmail.com>
2026-03-18 09:37:27 +0530

    18 march

--- practice.py
+++ practice.py
@@ -1,2 +1,10 @@
-#no coding today but git commit is must
-# same as above lil busy today also 
+import random
+import string
+
+def pass_gen(length=12):
+    char = string.ascii_letters + string.digits + string.punctuation + string.printable
+    passw = ''.join(random.choice(char) for i in range(length))
+    return passw
+
+print(f"your new secure password is {pass_gen(16000)}")
+
