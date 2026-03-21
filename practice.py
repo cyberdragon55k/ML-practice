9b02f95ba363c713a54a518903253187f2cda825
Cyberdragon <aditya5namdeo@gmail.com>
2026-03-21 21:50:06 +0530

    21 march

--- practice.py
+++ practice.py
@@ -1,1 +1,18 @@
-from collections import namedtuple
+add10 = lambda x:x +10
+print(add10(5))
+def add10_fun(x):
+    return x+10
+
+mult = lambda x,y:x*y
+# print(mult(3,10))
+
+
+points2d= [(1,2),(15,1),(5,-1),(10,4)]
+def sortbyy(x):
+    return x[1]
+
+points2d_sort = sorted(points2d, key=lambda x:x[0] +x[1])
+print(points2d)
+print(points2d_sort)
+
+
