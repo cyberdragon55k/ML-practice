7ca94aa342d1ef5c2a8abdd4918d325b39580a31
Cyberdragon <aditya5namdeo@gmail.com>
2026-03-26 22:08:42 +0530

    26 march

--- practice.py
+++ practice.py
@@ -1,26 +1,10 @@
-a = [1,2,3,4,5]
-# b = 2 *a
+# json
 
-b = map(lambda x:x*2 ,a)
-print(list(b))
-
-from functools import reduce
-a = [1,2,3,4,5,6]
-
-product_a = reduce(lambda x,y: x*y,a)
-
-print(product_a)
-
-#error and exceptions
-
-x = -5 
-if x < 0: 
-    raise Exception('x should be positive')
-# commit #1234567890000000
+import json
 
 
 
-#new code in copy note book 
 
-import logging
-logging.debug("this is a debuging msg")
+with open('Jupyter\ex.json','r') as file:
+    person = json.load(file)
+    print(person)
