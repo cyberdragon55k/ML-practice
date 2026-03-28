5ea3fc435fe5fb6a38b4bc02b6140f1694fdefc4
Cyberdragon <aditya5namdeo@gmail.com>
2026-03-28 21:33:05 +0530

    28 march

--- practice.py
+++ practice.py
@@ -1,4 +1,15 @@
-import random
+def start_end_decorator(func):
+    
+    def wrapper():
+        print("Start")
+        func()
+        print("End")
+    return wrapper 
 
-a = random.uniform(1,10)
-print(a) 
+@start_end_decorator
+def print_name():
+    print('aditya')
+
+# print_name = start_end_decorator(print_name)
+
+print_name()
