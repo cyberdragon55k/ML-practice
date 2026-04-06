9d828d020d532acac3e960a3b452f87900fe1500
Cyberdragon <aditya5namdeo@gmail.com>
2026-04-06 10:40:43 +0530

    6 april

--- practice.py
+++ practice.py
@@ -1,9 +1,27 @@
-import pandas as pd
-import numpy as np
+# decorators = a function that extends the behavior of another function w/o modify 
+# the base funtion pass the base function as an argument to the decoratot
+# 
+#   @add_sprinkes
+#   get_ice_cream(" vanilla")
+
+def add_sprinkles(func):
+    def wrapper():
+        print("you add sprinkles")
+        func()
+    return wrapper
 
 
-df = pd.read_csv('')
+def add_fud(func):
+    def wrapper():
+        print("you add fudge")
+        func()
+    return wrapper
 
+@add_sprinkles
+@add_fud
+def get_ice_cream():
+    print("here is you ice cream")
 
-# creating dataframe using csv 
+get_ice_cream()
 
+get_ice_cream()
