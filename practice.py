91586adc90ad5ae2448a33209c7a4c8168243946
Cyberdragon <aditya5namdeo@gmail.com>
2026-04-09 22:28:13 +0530

    122

--- practice.py
+++ practice.py
@@ -1,27 +1,11 @@
-# * args    =  allow you to pass multiple non key arguments
-# ** kwargs =  allow you to pass multiple keywords arguments 
+#threading and multiprocessing
 
 
-# def print_add(**kwargs):
-#     for value in kwargs.values():
-#         print(value)
+from multiprocessing import Process
+import os 
+processes = []
 
+num_processes = os.cpu_count{}
 
-# print_add(street="main road",
-#                 city="jbp",
-#                 state="mp",
-#                 zip="1234")
-
-
-def shipping_label(*args, **kwargs):
-    for arg in args:
-        print(arg, end=" ")
-    print()
-
-    for value in kwargs.values():
-        print(value, end=" ")
-
-shipping_label("DR ","SPONGEbOB",
-               street= "124",
-               apt="100",
-               state= 'MI')
+for i in range(num_processes):
+    p  = Process(target=)
