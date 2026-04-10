4c6b156be423cc753947fee80cdca98ad64852e2
Cyberdragon <aditya5namdeo@gmail.com>
2026-04-10 10:02:07 +0530

    multi threading

--- practice.py
+++ practice.py
@@ -1,11 +1,26 @@
 #threading and multiprocessing
 
 
-from multiprocessing import Process
+from threading import Thread
 import os 
-processes = []
+import time 
+def square_number():
+    for i in range(100):
+        i *i
+        time.sleep(0.1)
 
-num_processes = os.cpu_count{}
 
-for i in range(num_processes):
-    p  = Process(target=)
+thread = []
+
+num_thread = os.cpu_count()
+
+for i in range(num_thread):
+    p  = Thread(target=square_number)
+    thread.append(p) 
+
+for t in thread:
+    t.start()
+for t in thread:
+    t.join()
+
+print("end main")
