ad1ae3ae84f0415465b2aff495fa4ce174236d76
Cyberdragon <aditya5namdeo@gmail.com>
2026-04-17 10:22:48 +0530

    111

--- practice.py
+++ practice.py
@@ -1,15 +1,1 @@
-def fo(a,b=2,*args, **kwargs):
-    print(a,b)
-    for arg in args:
-        print(args)
-    for key in kwargs:
-        print(key,kwargs[key])
-
-
-fo(1, 2, 3, 4,5,six=6,seven=7)
-
-
-def add(a,b,c):
-    print(a + b -c)
-
-# new code from here
+aaaa
