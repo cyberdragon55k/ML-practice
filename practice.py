336ce19a3fa15c86534c8bef5f9d3dde232908c2
Cyberdragon <aditya5namdeo@gmail.com>
2026-04-26 22:44:51 +0530

    26 aprile

--- practice.py
+++ practice.py
@@ -1,1 +1,19 @@
-aaaa
+class employee:
+    company_name= "tech sol"
+    def __init__(self,name,base_salary):
+        self.name= name
+        self.__base_salary = base_salary
+
+    def get_salary(self):
+        return self.__base_salary
+    def give_raise(self,amount):
+        if amount > 0:
+            self.__base_salary+= amount
+            print(f"paise of ${amount} approved for {self.name}.")
+
+        else:
+            print("invalid raise amount.")
+
+    def work(self):
+        return f"{self.name} is performing general corporate duties"
+
