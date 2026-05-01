0f39d11ad12e585eec8d6a2ab2181006f0e11af0
Cyberdragon <aditya5namdeo@gmail.com>
2026-05-01 21:21:39 +0530

    oop

--- practice.py
+++ practice.py
@@ -1,1 +1,10 @@
-11111111111111
+class libarayitem:
+    def __init__(self, title ,creator):
+        self.title = title
+        self.creator = creator
+        self._is_checked_out = False
+
+    def display_info(self):
+        status = "checked out " if self._is_checked_out else "available"
+        return f"'{self.title}' by {self.creator} - [{status}]"
+    
