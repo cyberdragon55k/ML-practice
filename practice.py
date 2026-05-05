09a868991893bb522662eea0512051cbf07093fe
Cyberdragon <aditya5namdeo@gmail.com>
2026-05-05 21:06:01 +0530

    12345

--- practice.py
+++ practice.py
@@ -1,6 +1,14 @@
-import pandas as pd 
+class Robot:
+    population = 0
 
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+        Robot.population += 1
 
+    
+    def say_hello(self):
+        return f"Greetings. I am {self.name}, version {self.version}."
 
-
-df = pd.array(1,1)
+r1 = Robot("R2-D2", 1.0)
+print(r1.say_hello())
