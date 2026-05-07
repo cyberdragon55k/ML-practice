9d75acb8c5462e978c127138e3eca9498d912028
Cyberdragon <aditya5namdeo@gmail.com>
2026-05-07 21:26:48 +0530

    @222:

--- practice.py
+++ practice.py
@@ -22,4 +22,53 @@ print("aditya was here ")
 
 print("111111111111111111111111111111" \
 "" \
+")class Robot:
+    population = 0
+
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+        Robot.population += 1
+
+    
+    def say_hello(self):
+        return f"Greetings. I am {self.name}, version {self.version}."
+
+r1 = Robot("R2-D2", 1.0)
+print(r1.say_hello())
+
+
+
+print("aditya was here ")
+
+
+
+
+print("111111111111111111111111111111" \
+"" \
+")
+class Robot:
+    population = 0
+
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+        Robot.population += 1
+
+    
+    def say_hello(self):
+        return f"Greetings. I am {self.name}, version {self.version}."
+
+r1 = Robot("R2-D2", 1.0)
+print(r1.say_hello())
+
+
+
+print("aditya was here ")
+
+
+
+
+print("111111111111111111111111111111" \
+"" \
 ")
