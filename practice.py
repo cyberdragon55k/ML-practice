e6b256f1c612fb7e62aeaa1896af609ddfbbd900
Cyberdragon <aditya5namdeo@gmail.com>
2026-05-15 21:12:54 +0530

    2222

--- practice.py
+++ practice.py
@@ -22,3 +22,51 @@ my_list = []
 
 my_list = my_func(my_list,5)
 print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
