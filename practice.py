309b17a9123d0f02b5ff4aafadc20e93b134c632
Cyberdragon <aditya5namdeo@gmail.com>
2026-05-16 22:11:17 +0530

    11

--- practice.py
+++ practice.py
@@ -70,3 +70,75 @@ my_list = []
 
 my_list = my_func(my_list,5)
 print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
+my_list = []
+
+my_list = my_func(my_list,5)
+print(my_list)
